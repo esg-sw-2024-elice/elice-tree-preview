ab581d869eb21ae7c1cdd98eb7840ec2d16195bb
Byeolchan Kim <qufstar46@gmail.com>
2024-11-21 11:59:44 +0900

    feat: apply font globally

--- elice-tree-fe/src/App.tsx
+++ elice-tree-fe/src/App.tsx
@@ -1,10 +1,15 @@
 import { Router } from 'components';
 import React from 'react';
+import GlobalStyle from 'styles/global/GlobalStyle';
+import { FontContainer } from 'styles/global/index.styles';
 
 function App() {
   return (
     <div className="App">
-      <Router />
+      <GlobalStyle />
+      <FontContainer>
+        <Router />
+      </FontContainer>
     </div>
   );
 }
