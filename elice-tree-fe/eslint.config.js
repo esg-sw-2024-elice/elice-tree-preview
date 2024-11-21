7b50534db0f07102bdb184cc52f8c360105dedb4
Byeolchan Kim <qufstar46@gmail.com>
2024-11-21 21:00:09 +0900

    infra: set up eslint

---
+++ elice-tree-fe/eslint.config.js
@@ -0,0 +1,28 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
+
+export default tseslint.config([
+  { ignores: ['build'] },
+  {
+    extends: [
+      pluginJs.configs.recommended,
+      ...tseslint.configs.recommended,
+      pluginReact.configs.flat.recommended,
+    ],
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+    },
+  },
+]);
